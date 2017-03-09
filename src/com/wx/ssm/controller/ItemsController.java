3b0fb81e6978fe7af21422197fcb4655bc89763b
WangSanPing <wangxu_1987@126.com>
2017-03-09 17:27:06 +0800

    create

---
+++ src/com/wx/ssm/controller/ItemsController.java
@@ -0,0 +1,17 @@
+package com.wx.ssm.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ItemsController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		return null;
+	}
+
+}
